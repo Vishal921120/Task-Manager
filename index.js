b284cd3fb7f0ea29276276f7d4d8bb8e1bc432bd
Vishal921120 <vishaldabas04@gmail.com>
2021-06-30 01:24:21 +0530

    add delete feature

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const taskContainer = document.querySelector(".task__container");
 
-const globalStore = []; //some values []
+let globalStore = []; //some values []
 
 const generateNewCard = (taskData) => `
-<div class="col-md-6 col-lg-4" id=${taskData.id}>
+<div class="col-md-6 col-lg-4" >
 <div class="card">
     <div class="card-header d-flex justify-content-end gap-2">
         <button type="button" class="btn btn-outline-success"><i class="fas fa-pencil-alt"></i></button>
-        <button type="button" class="btn btn-outline-danger"><i class="fas fa-trash-alt"></i></button>
+        <button type="button" class="btn btn-outline-danger" id=${taskData.id} onclick="deleteCard.apply(this,arguments)">
+        <i class="fas fa-trash-alt" id=${taskData.id} onclick="deleteCard.apply(this,arguments)"></i></button>
     </div>
     <img src=${taskData.imageUrl} class="card-img-top" alt="...">
     <div class="card-body">
@@ -39,10 +40,6 @@ const loadInitialCardData = () => {
     })
 };
 
-
-
-
-
 const saveChanges = () => {
     const taskData = {
         id:`${Date.now()}`, //unique number for id        
@@ -59,3 +56,24 @@ const saveChanges = () => {
     localStorage.setItem("tasky",JSON.stringify({cards:globalStore}));
 
 };
+
+const deleteCard = (event) => {
+    event = window.event;
+    // id 
+    const targetID = event.target.id;
+    const tagname = event.target.tagName;
+
+    // match the id of the element with the id inside the globalStore
+    // if match found remove 
+
+    globalStore = globalStore.filter((cardObject) => cardObject.id !== targetID);
+    localStorage.setItem("tasky",JSON.stringify({cards:globalStore}));
+    
+    //contact parent 
+    
+    if(tagname === "BUTTON"){
+        return taskContainer.removeChild(event.target.parentNode.parentNode.parentNode);
+    }else{
+        return taskContainer.removeChild(event.target.parentNode.parentNode.parentNode.parentNode);
+    }
+    }
